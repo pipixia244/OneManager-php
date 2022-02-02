9f8d9f46da04a1aef463edbf0279ca537407e5fd
pipixia244 <yukinon@srpr.cc>
2022-02-02 19:06:43 +0800

    Unified Styles

--- common.php
+++ common.php
@@ -480,7 +480,7 @@ function main($path)
             $files['error']['code'] = 'unknownError';
             $files['error']['stat'] = 500;
         }
-        return message('<div style="margin:8px;"><pre>' . $files['error']['message'] . '</pre></div><a href="javascript:history.back(-1)">'.getconstStr('Back').'</a>', $files['error']['code'], $files['error']['stat']);
+        return message('<div style="margin:2px;"><pre>' . $files['error']['message'] . '</pre></div><a href="javascript:history.back(-1)">'.getconstStr('Back').'</a>', $files['error']['code'], $files['error']['stat']);
     }
 }
 
@@ -930,16 +930,99 @@ function message($message, $title = 'Message', $statusCode = 200, $wainstat = 0)
     $html = '
 <html lang="' . $_SERVER['language'] . '">
 <html>
-    <meta charset=utf-8>
-    <meta name=viewport content="width=device-width,initial-scale=1">
-    <body>
-        <a href="' . $_SERVER['base_path'] . '">' . getconstStr('Back') . getconstStr('Home') . '</a>
-        <h1>' . $title . '</h1>
+<head>
+<meta charset=utf-8>
+<meta name="viewport"
+      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no"/>
+<link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css">
+
+
+<style type="text/css">
+        body{font-family:\'Helvetica Neue\',Helvetica,Arial,sans-serif;font-size:14px;line-height:1em;color:#000;background-color:#f7f7f9;background-repeat:no-repeat;background-size:cover;background-attachment:fixed;background-image:url("https://od.srpr.cc/sust0/mashiro2.jpg");}
+        ion-icon{font-size:15px;vertical-align:bottom}
+        h1, h2, h3, h4, h5, h6,
+        .h1, .h2, .h3, .h4, .h5, .h6{font-weight:bolder}
+        .tabs{width:100%;}
+        .changelanguage{position:absolute;right:5px;}
+        .title{text-align:center;margin-top:1rem;letter-spacing:2px;margin-bottom:2rem}
+        .title a{color:#333;text-decoration:none}
+        .list-wrapper{width:80%;margin:0 auto 30px;position:relative;box-shadow:0 0 32px 0 rgb(128,128,128);border-radius:15px;}
+        .list-container{position:relative;/*overflow:hidden;*/border-radius:15px;background-color:rgba(245,245,245,1);}
+        .list-container,.list-header-container,.list-wrapper,a.back-link:hover,body{color:#24292e}
+        .list-header-container{position:relative}
+        .list-header-container a.back-link{color:#000;display:inline-block;position:absolute;font-size:16px;margin:20px 10px;padding:10px 10px;vertical-align:middle;text-decoration:none}
+        .table-header{margin:0;border:0 none;padding:30px 30px;text-align:left;font-weight:400;color:#000;word-break: break-all;word-wrap: break-word;}
+        .list-body-container{position:relative;left:0;box-sizing:border-box;/*overflow-x:hidden;overflow-y:auto;background:rgba(245,245,245,1)*/}
+        .more-disk{margin:0;border:0 none;padding:30px 30px;text-align:left;font-weight:400;color:#000;white-space:nowrap;overflow:auto;}
+        .more-disk a{margin:0 10px;padding:5px;transition-duration: 0.4s;border-radius: 12px; background-color: white; color: black; border: 2px solid rgba(85,85,85,0.7); }
+        .more-disk a:hover, .more-disk a[now]{ background-color: rgba(85,85,85,0.7); color: white; }
+        .list-table{width:100%;padding:0 20px 20px 20px;border-spacing:0}
+        .list-table tr{height:40px}
+        .list-table tr[data-to]:hover{background:rgba(85,85,85,0.7) !important;color:white;}
+        .list-table tr[data-to]:hover a{color:white}
+        .list-table tr:first-child{background:rgba(245,245,245,0)}
+        .list-table td,.list-table th{padding:0 10px;text-align:left}
+        .list-table .size,.list-table .updated_at{text-align:right}
+        .updated_at{width:25%}
+        .size{width:15%}
+        .mask{position:absolute;left:0px;top:0px;width:100%;background-color:#000;filter:alpha(opacity=50);opacity:0.5;z-index:2;}
+
+        .operate{display:inline-table;margin:0;margin-right:5px;list-style:none}
+        .operate ul{position:absolute;display:none;background:#fffaaa;border:0px #f7f7f7 solid;border-radius:5px;margin:-7px 0 0 0;padding:0 7px;color:#205D67;z-index:1;}
+        .operate:hover ul{position:absolute;display:inline-table;}
+        body .active ul{position:absolute;display:inline-table;} /*for some mobile browser */
+        .operate ul li{padding:7px;list-style:none;display:block;}
+        .list-table tr[data-to]:hover .operate ul li a{color:black}
+
+        .operatediv{position:absolute;border:1px #CCCCCC;background-color:#FFFFCC;z-index:2;}
+        .operatediv div{margin:16px}
+        .operatediv_close{position:absolute;right:3px;top:3px;}
+        .readme{padding:8px;}
+        .markdown-body, .customfile{padding:20px;text-align:left}
+        @media only screen and (orientation: portrait){
+
+            body{background-repeat:no-repeat;background-size:cover;background-attachment:fixed;background-image:url("https://od.srpr.cc/sust0/mashiro2.jpg");}
+
+            .title{margin-bottom:24px}
+            .list-wrapper{width:95%; margin-bottom:24px;}
+            .list-table {padding:8px}
+            .list-table td, .list-table th{white-space:nowrap;overflow:auto;max-width:80px}
+            .size, .updated_at{display:none}
+        }
+</style>
+
+</head>
+<body>
+
+<div style="padding:8px" id="top-div"></div>
+
+<h2 class="title" id="title">
+        <a href="/">Pipixia244\'s Share Station</a>
+</h2>
+
+<div class="list-wrapper" id="list-div">
+<div class="list-container">
+<div class="list-header-container">
+
+<div style="padding:10px" id="top-div"></div>
+<main class="main-content template-error-404">
+  <div class="container-sm">
+    <div class="row">
+      <section class="d-flex flex-column justify-content-center align-items-center w-100 error-message">
+        <div style="padding:4px" id="top-div"></div>
+        <h3 class="error-code">' . $title . '</h3>
+        ' . $message . '
+         <div style="padding:8px" id="top-div"></div>
+        <a class="error-link" href="' . $_SERVER['base_path'] . '">'. getconstStr('Back') . getconstStr('Home') . '</a>
         <div id="dis" style="display: none;">
+        <div style="padding:6px" id="top-div"></div>
+      </section>
+    </div>
+  </div>
+</main>
 
-' . $message . '
-
-        </div>';
+</div></div></div>
+';
     if ($wainstat) {
         $html .= '
         <div id="err"></div>
@@ -985,8 +1068,34 @@ function message($message, $title = 'Message', $statusCode = 200, $wainstat = 0)
         <script>document.getElementById("dis").style.display = "";</script>';
     }
     $html .= '
+    <footer class="main-footer">
+    <div class="container d-flex justify-content-md-between justify-content-center">
+      <div class="text-center main-footer-copyright">
+        <p>Powered by <a href="https://github.com/qkqpttgf/OneManager-php" rel="noopener nofollow" target="_blank">OneManager</a>.
+        </p>
+      </div>
+      <div class="d-none d-md-block main-footer-meta">只争朝夕，不负韶华。</div>
+    </div>
+    <div
+      class="container d-flex flex-wrap-reverse justify-content-md-between justify-content-center text-center main-footer-audit">
+      <p>
+      </p>
+    </div>
+    </footer>
+    </div>
+    
+    </div>
+    <div class="toast-wrapper" aria-live="polite" aria-atomic="true">
+    <div class="toast-wrapper-list"></div>
+    </div>
+    
+    
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js"></script>
+    <script src="https://cdn.bootcss.com/jquery.pjax/2.0.1/jquery.pjax.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/scrollreveal@4.0.5/dist/scrollreveal.min.js"></script>
     </body>
-</html>
+    </html>
 ';
     return output($html, $statusCode);
 }
